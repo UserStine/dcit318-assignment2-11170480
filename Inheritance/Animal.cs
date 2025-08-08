fa984fc9afeff78e9963b007b346889e7c59d04b
UserStine <justineaddo15@gmail.com>
2025-08-08 22:14:53 +0000

    Create Animal Project

---
+++ Inheritance/Animal.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_2.Inheritance
+{
+    namespace Assignment2.Inheritance
+    {
+        public class Animal
+        {
+            public virtual string MakeSound() => "Some generic sound";
+        }
+
+        public class Dog : Animal
+        {
+            public override string MakeSound() => "Bark";
+        }
+
+        public class Cat : Animal
+        {
+            public override string MakeSound() => "Meow";
+        }
+    }
+
+}

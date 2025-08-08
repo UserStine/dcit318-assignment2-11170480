70e2ceb57bba01f82c0bd230d8d49e20e1fe2123
UserStine <justineaddo15@gmail.com>
2025-08-08 22:15:34 +0000

    Create Imovable Project

---
+++ Interface/IMovable.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_2.Interface
+{
+    namespace Assignment2.Interfaces
+    {
+        public interface IMovable
+        {
+            string Move();
+        }
+
+        public class Car : IMovable
+        {
+            public string Move()
+            {
+                return "Car is moving";
+            }
+        }
+
+        public class Bicycle : IMovable
+        {
+            public string Move()
+            {
+                return "Bicycle is moving";
+            }
+        }
+    }
+
+}

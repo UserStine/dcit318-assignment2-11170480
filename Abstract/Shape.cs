c34db662ab764888aff141cad8893b80da15a46b
UserStine <justineaddo15@gmail.com>
2025-08-08 22:14:19 +0000

    Create Shape Project

---
+++ Abstract/Shape.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_2.Abstract
+{
+    using System;
+
+    namespace Assignment2.Abstrac
+    {
+        public abstract class Shape
+        {
+            public abstract double GetArea();
+        }
+
+        public class Circle : Shape
+        {
+            public double Radius { get; set; }
+
+            public Circle(double radius)
+            {
+                Radius = radius;
+            }
+
+            public override double GetArea()
+            {
+                return Math.PI * Radius * Radius;
+            }
+        }
+
+        public class Rectangle : Shape
+        {
+            public double Length { get; set; }
+            public double Width { get; set; }
+
+            public Rectangle(double length, double width)
+            {
+                Length = length;
+                Width = width;
+            }
+
+            public override double GetArea()
+            {
+                return Length * Width;
+            }
+        }
+    }
+
+}
